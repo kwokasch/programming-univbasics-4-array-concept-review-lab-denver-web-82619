18512d41208092a9d85972ad67a2d100e70f5bcf
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-31 15:31:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  counter = 0 
+  while counter < array.length do
+    return array.max()   #--> find index number of input element 
+    counter +=1 
+  end
 end
 
 def find_min_value(array)
